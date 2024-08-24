df21b375a733755c02df1a3794ab84cadda55564
Profxbaba <daddyslove41@gmail.com>
2024-08-24 22:18:33 +0000

    Making Change

---
+++ 0x08-making_change/making_change.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module for makeChange function.
+"""
+
+def makeChange(coins, total):
+    """
+    Determines the fewest number of coins needed to meet a given amount total.
+
+    Args:
+        coins (list): A list of the values of the coins available.
+        total (int): The total amount to achieve.
+
+    Returns:
+        int: The fewest number of coins needed to meet the total.
+             Returns 0 if total is 0 or less.
+             Returns -1 if total cannot be met by any number of coins.
+    """
+    if total <= 0:
+        return 0
+
+    # Initialize dp array where dp[i] will be the fewest number of coins needed for amount i
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
