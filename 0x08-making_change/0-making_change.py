00fc31ea7af7d15d1d04b96b3ca2c9ad3a5a6917
Profxbaba <daddyslove41@gmail.com>
2024-08-24 22:46:02 +0000

    Making Changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -24,9 +24,9 @@ def makeChange(coins, total):
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
+    # Compute the minimum coins needed for each amount from 1 to total
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
