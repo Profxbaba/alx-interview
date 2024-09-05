2b4ff78ea407271f385a3a369380d253b41dcb4d
Profxbaba <daddyslove41@gmail.com>
2024-09-05 19:07:47 +0000

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -28,33 +28,27 @@ def isWinner(x, nums):
     """
     Determine who is the winner after x rounds of prime game.
     Maria goes first, and both play optimally.
-    
     Args:
     x (int): Number of rounds
     nums (list): List of numbers (n) for each round
-    
     Returns:
     str: Name of the player with the most wins or None
     """
     if not nums or x < 1:
         return None
-    
     # Precompute primes up to the maximum value in nums
     max_num = max(nums)
     primes = sieve_of_eratosthenes(max_num)
-    
     maria_wins = 0
     ben_wins = 0
-    
     for n in nums:
         prime_count = count_prime_multiples(n, primes)
         if prime_count % 2 == 0:
             ben_wins += 1
         else:
             maria_wins += 1
-    
-    if maria_wins > ben_wins:
-        return "Maria"
+        if maria_wins > ben_wins:
+            return "Maria"
     elif ben_wins > maria_wins:
         return "Ben"
     else:
